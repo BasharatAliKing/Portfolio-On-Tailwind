8610dfb7ef0a097c4af411cdb6fa5c9992fc7bfc
Basharat Ali <basharatalidogar14@gmail.com>
2023-11-27 07:00:02 +0500

    data added

--- src/Layouts/Navbar.jsx
+++ src/Layouts/Navbar.jsx
@@ -32,10 +32,10 @@ export default function Navbar() {
       <nav  className=' text-white  z-10 w-full'>
         <div className='container flex flex-col md:flex-row items-center justify-between'>
         <div className="logo"><img src={process.env.PUBLIC_URL+"./logo.png"} className='w-[30%]' alt="my-pic" /></div>
-        <div className='text-xl absolute md:hidden right-8 top-6' onClick={()=>{setOpen(!Open)}}>
+        <div className='text-xl absolute md:hidden right-8 top-8' onClick={()=>{setOpen(!Open)}}>
         <ion-icon name={Open ? "close":"menu"}></ion-icon>
         </div>
-         <ul className={`md:flex items-start bg-black w-full md:items-center absolute md:z-auto z-50  left-0 md:w-auto md:static top-20 transition-all duration-500 ease-in py-10 md:py-0 ${ Open ? 'top-20 opacity-100' : 'top-[-500px]'} `} > 
+         <ul className={`md:flex items-start bg-black w-full md:items-center absolute md:z-auto z-50  left-0 md:w-auto md:static top-20 transition-all duration-300 ease-in py-10 md:py-0 ${ Open ? 'top-20 opacity-100' : 'top-[-500px]'} `} > 
             {Links.map((link)=>(
               <li key={link.name} className='ml-8 mt-3 md:mt-0 text-xl hover:duration-500 hover:text-red font-saira font-semibold'>
                 <a href={link.link} >{link.name}</a></li>
